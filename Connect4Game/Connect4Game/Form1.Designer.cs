a921aa99c1d95bd0611e10b5d12a6f193b59b8ae
Mateusz Chiliński <mateusz@chilinski.eu>
2019-04-10 21:31:46 +0200

    initial commit

---
+++ Connect4Game/Connect4Game/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Connect4Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Connect4Board = new System.Windows.Forms.PictureBox();
+            this.BlackRadioButton = new System.Windows.Forms.RadioButton();
+            this.ChooseLabel = new System.Windows.Forms.Label();
+            this.RedRadioButton = new System.Windows.Forms.RadioButton();
+            this.RestartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Connect4Board)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Connect4Board);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.BlackRadioButton);
+            this.splitContainer1.Panel2.Controls.Add(this.ChooseLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.RedRadioButton);
+            this.splitContainer1.Panel2.Controls.Add(this.RestartButton);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 615;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Connect4Board
+            // 
+            this.Connect4Board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Connect4Board.Location = new System.Drawing.Point(0, 0);
+            this.Connect4Board.Name = "Connect4Board";
+            this.Connect4Board.Size = new System.Drawing.Size(615, 450);
+            this.Connect4Board.TabIndex = 0;
+            this.Connect4Board.TabStop = false;
+            this.Connect4Board.Click += new System.EventHandler(this.Connect4Board_Click);
+            this.Connect4Board.MouseHover += new System.EventHandler(this.Connect4Board_MouseHover);
+
+            // 
+            // BlackRadioButton
+            // 
+            this.BlackRadioButton.AutoSize = true;
+            this.BlackRadioButton.Location = new System.Drawing.Point(2, 106);
+            this.BlackRadioButton.Name = "BlackRadioButton";
+            this.BlackRadioButton.Size = new System.Drawing.Size(73, 21);
+            this.BlackRadioButton.TabIndex = 3;
+            this.BlackRadioButton.Text = "Czarny";
+            this.BlackRadioButton.UseVisualStyleBackColor = true;
+            this.BlackRadioButton.Click += new System.EventHandler(this.BlackRadioButton_CheckedChanged);
+            // 
+            // ChooseLabel
+            // 
+            this.ChooseLabel.AutoSize = true;
+            this.ChooseLabel.Location = new System.Drawing.Point(3, 59);
+            this.ChooseLabel.Name = "ChooseLabel";
+            this.ChooseLabel.Size = new System.Drawing.Size(98, 17);
+            this.ChooseLabel.TabIndex = 2;
+            this.ChooseLabel.Text = "Wybierz kolor:";
+            // 
+            // RedRadioButton
+            // 
+            this.RedRadioButton.AutoSize = true;
+            this.RedRadioButton.Checked = true;
+            this.RedRadioButton.Location = new System.Drawing.Point(2, 79);
+            this.RedRadioButton.Name = "RedRadioButton";
+            this.RedRadioButton.Size = new System.Drawing.Size(90, 21);
+            this.RedRadioButton.TabIndex = 1;
+            this.RedRadioButton.TabStop = true;
+            this.RedRadioButton.Text = "Czerwony";
+            this.RedRadioButton.UseVisualStyleBackColor = true;
+            this.RedRadioButton.Click += new System.EventHandler(this.RedRadioButton_CheckedChanged);
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(39, 12);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(110, 35);
+            this.RestartButton.TabIndex = 0;
+            this.RestartButton.Text = "Restartuj gre";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Connect4Board)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox Connect4Board;
+        private System.Windows.Forms.RadioButton BlackRadioButton;
+        private System.Windows.Forms.Label ChooseLabel;
+        private System.Windows.Forms.RadioButton RedRadioButton;
+        private System.Windows.Forms.Button RestartButton;
+    }
+}
+
---
+++ Connect4Game/Connect4Game/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4Game
+{
+    public partial class Form1 : Form
+    {
+        const int rows = 6;
+        const int columns = 7;
+        const int margin = 10;
+
+        PlayerColor UserPlayer = PlayerColor.Red;
+        FieldType[,] Board = new FieldType[rows,columns]; // row, column
+        GameStatusType GameStatus = GameStatusType.Initialised;
+        enum GameStatusType
+        {
+            Initialised = 0,
+            Started = 1,
+            RedWin = 2,
+            BlackWin = 3
+        }
+        enum FieldType
+        {
+            Empty = 0,
+            Red = 1, // red always starts
+            Black = 2
+        };
+        public enum PlayerColor
+        {
+            Red = 0,
+            Black = 1
+        }
+        private void DrawConnect4(Graphics graphics)
+        {
+            int MaxWidth = Connect4Board.Size.Width;
+            int MaxHeight = Connect4Board.Size.Height;
+
+            
+            
+            float UsedWidthPerOne = (MaxWidth - 2 * margin)/(float)columns;
+            float UsedHeightPerOne = (MaxHeight - 2 * margin)/(float)rows;
+
+            for(int i = 0; i <= columns; i++)
+            {
+                graphics.DrawLine(Pens.Black, margin + UsedWidthPerOne * i, margin, margin + UsedWidthPerOne * i, MaxHeight - margin);
+            }
+
+            for (int i = 0; i <= rows; i++)
+            {
+                graphics.DrawLine(Pens.Black, margin, margin + UsedHeightPerOne * i, MaxWidth - margin, margin + UsedHeightPerOne * i);
+            }
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    var field = Board[i, j];
+                    if (field == FieldType.Empty)
+                        continue;
+                    if(field == FieldType.Red)
+                    {
+                        DrawCircle(graphics, (int)UsedWidthPerOne, (int)UsedHeightPerOne, margin, 2, 2, Brushes.Red);
+                    }
+                    else
+                        DrawCircle(graphics, (int)UsedWidthPerOne, (int)UsedHeightPerOne, margin, 2, 2, Brushes.Black);
+                }
+            }
+
+            
+        }
+        private void DrawCircle(Graphics graphics, int UsedWidthPerOne, int UsedHeightForOne, int margin, int row, int column, Brush brush)
+        {
+            graphics.FillEllipse(brush, new Rectangle(UsedWidthPerOne * row + (int)(margin * 1.5), UsedHeightForOne * column + (int)(margin * 1.5), 
+                UsedWidthPerOne - margin, UsedHeightForOne - margin));
+
+        }
+        public bool PutInColumn(int Column, PlayerColor playerColor)
+        {
+            if(GameStatus == GameStatusType.RedWin || GameStatus == GameStatusType.BlackWin) // if game has finished one cannot put more
+                return false;
+            if (Board[Board.GetLength(0)-1, Column] != FieldType.Empty) // if column is full, not possible
+                return false;
+
+            for(int i = 0; i < Board.GetLength(0); i++)
+            {
+                if(Board[i,Column] == FieldType.Empty)
+                {
+                    if (playerColor == PlayerColor.Red)
+                        Board[i, Column] = FieldType.Red;
+                    else
+                        Board[i, Column] = FieldType.Black;
+                    break;
+                }
+            }
+
+            return true;
+        }
+        private void RestartGame()
+        {
+            GameStatus = GameStatusType.Initialised;
+            for(int i = 0; i < Board.GetLength(0); i++)
+            {
+                for(int j = 0; j < Board.GetLength(1); j++)
+                {
+                    var field = Board[i, j];
+                    field = FieldType.Empty;
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Connect4Board.Paint += Connect4Board_Paint;
+        }
+
+        private void Connect4Board_Paint(object sender, PaintEventArgs e)
+        {
+            DrawConnect4(e.Graphics);
+        }
+
+        private void ChangeToColor()
+        {
+            if(GameStatus == GameStatusType.Started)
+            {
+                DialogResult dialogResult = MessageBox.Show("Gra została już rozpoczęta. Zmiana koloru spowoduje restart gry. Czy chcesz zmienić kolor?", "Gra już rozpoczęta!", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.No)
+                {
+                    RedRadioButton.Checked = !RedRadioButton.Checked;
+                    BlackRadioButton.Checked = !BlackRadioButton.Checked;
+                    return;
+                }
+            }
+            if (RedRadioButton.Checked)
+                UserPlayer = PlayerColor.Red;
+            if (BlackRadioButton.Checked)
+                UserPlayer = PlayerColor.Black;
+            RestartGame();
+        }
+
+        private void RedRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (UserPlayer == PlayerColor.Red && RedRadioButton.Checked)
+                return;
+            ChangeToColor();
+        }
+
+        private void BlackRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (UserPlayer == PlayerColor.Black && BlackRadioButton.Checked)
+                return;
+            ChangeToColor();
+        }
+
+        private void Connect4Board_Click(object sender, EventArgs e)
+        {
+            int X = MousePosition.X;
+        }
+
+        private Tuple?<int,int> GetAvailableInRow(int x)
+        {
+            int MaxWidth = Connect4Board.Width - 2 * margin;
+            int UsedWidthPerOne = (int)((MaxWidth - 2 * margin) / (float)columns);
+            int row = 0;
+            if (x < margin)
+                return null;
+            for (int i = margin; i < MaxWidth; i+= UsedWidthPerOne)
+            {
+                if (x > i && x < i + UsedWidthPerOne)
+                {
+                    break;
+                }
+                row++;
+            }
+
+        }
+
+        private void Connect4Board_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
